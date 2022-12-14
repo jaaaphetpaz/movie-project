a4bb8c365b93ac96341096de87c44a018294a08d
jaaaphetpaz <japhetpaz17@gmail.com>
2022-12-14 11:11:21 -0500

    Nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import jpzLogo from "../assets/jpzLogo.png";
+import "./style.css";
+
+const Nav = () => {
+  return (
+    <div className="navbar">
+      <div className="nav__content-wrapper">
+        <div className="logo">
+          <img
+            style={{ width: "auto", height: "100px" }}
+            src={jpzLogo}
+            alt=""
+          />
+        </div>
+        <ul className="linkz">
+          <li className="link">
+            Home<i className="fas fa-home"></i>
+          </li>
+          <li className="link">
+            Movies<i className="fas fa-film"></i>
+          </li>
+          <li className="link">
+            TV Shows<i className="fas fa-tv"></i>
+          </li>
+          <li className="link">
+            Random<i className="fas fa-random"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
